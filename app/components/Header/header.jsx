73812faec786a594e32ec0258e0b41f20608f0e4
isactes <fidel.renovato@gmail.com>
2023-06-12 23:49:31 -0600

    add querys meia in css

--- app/components/Header/header.jsx
+++ app/components/Header/header.jsx
@@ -1,5 +1,7 @@
+'use client'
 import Link from 'next/link'
 import styles from './Header.module.css'
+import { useState } from 'react'
 
 const links = [{
   label: 'Home',
@@ -23,6 +25,12 @@ const links = [{
 }
 ]
 function Header () {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const handleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
   return (
     <header className={styles.menu}>
       <nav className={styles.header}>
@@ -32,9 +40,11 @@ function Header () {
               My web
             </Link>
           </h2>
-          <span className={styles.btnmenu}><i className={styles.fas && styles.fabars} /></span>
+          <span className={styles.btnmenu} onClick={handleMenu}>
+            <i className={isMenuOpen ? `${styles.fas}${styles.faTimes}` : `${styles.fas} ${styles.faBars}`} />
+          </span>
         </div>
-        <div className={styles.listcontainer}>
+        <div className={`${styles.listcontainer} ${isMenuOpen ? styles.showMenu : ''}`}>
           <ul className='lists'>
             {links.map(({ label, route }) => (
               <li className={styles.links} key={route}>
