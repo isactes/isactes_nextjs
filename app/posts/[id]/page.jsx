49cbd4785f05a8f686a43948e7f4d348edb648ae
isactes <fidel.renovato@gmail.com>
2023-06-13 22:15:00 -0600

    ingresar al post deseado junto con el id

--- app/posts/[id]/page.jsx
+++ app/posts/[id]/page.jsx
@@ -1,9 +1,18 @@
-function Post ({ params }) {
+const fetchSinglePosts = (id) => {
+  return fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, { cache: 'no-store' })
+    .then(res => res.json())
+}
+
+async function Post ({ params }) {
   const { id } = params
+  const post = await fetchSinglePosts(id)
   return (
-    <h1>
-      Este es un post {id}
-    </h1>
+    <article>
+      <h1>{post.title}</h1>
+      <div>
+        {post.body}
+      </div>
+    </article>
   )
 }
 export default Post
